158e5682649b961e421ed6fb3511c0e2f164624a
dionathan28 <dionathan_moreira@estudante.sesisenai.org.br>
2026-02-12 10:52:22 +0000

    UPDAT

---
+++ Atividades Python/atividade1.py
@@ -0,0 +1,6 @@
+#Primeira atividade de python usand concatenação de valores.
+print("Olá Mundo")
+numero1= input("Digite um numero: 1")
+numero2 input("Digite um numero: 2")
+soma = numero1=numero2
+print("a soma do numero 1 e 2 é: ",soma)
