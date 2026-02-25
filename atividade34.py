230f2dad59db78dde43258362155c97f785509be
dionathan28 <dionathan_moreira@estudante.sesisenai.org.br>
2026-02-25 14:20:13 +0000

    update

---
+++ atividade34.py
@@ -0,0 +1,4 @@
+quadrado1 = float(input("insira a o tamanho do lado do quadrado"))
+quadrado2 = float(input("insira a o tamanho do lado do quadrado"))
+A= quadrado1 * quadrado2
+print(f"A araea do quadrado é de {A}")  
